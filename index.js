54b64c7450c7978c6b3b3fc074612b76c7dcb12b
JEMcats <jakobmattheis@icloud.com>
2025-03-29 20:37:34 -0400

    Organize Code

---
+++ index.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    mainWindow.loadFile('UI/index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        mainWindow = new BrowserWindow({
+            width: 800,
+            height: 600,
+            webPreferences: {
+                nodeIntegration: true,
+            },
+        });
+
+        mainWindow.loadFile('UI/index.html');
+    }
+});
